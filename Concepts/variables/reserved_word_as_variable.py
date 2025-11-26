08fbf461ad47114173c63a372134ad733475ea1a
ayousocrazy <ayousocrazy@gmail.com>
2025-11-26 16:44:05 +0545

    Make different file for different variable topic

---
+++ Concepts/variables/reserved_word_as_variable.py
@@ -0,0 +1,4 @@
+print = "print"
+# This does not return error but now you cannot use print() as you could above as now print is a string not a function
+print(abc)
+# print(abc) does not work as print has been over written i.e. now Python interpreter thinks this print is a variable name and nothing else
---
+++ Concepts/variables/variable_type.py
@@ -0,0 +1,10 @@
+# You can check the type of variable using type(variable)
+number, sigma =  67, "male"
+
+print (type(number), type(sigma))
+"""
+Output:
+<class 'int'> <class 'str'>
+"""
+# the variable type is in class because everything in python is an object and 67 is an instance(like child) of class int
+
