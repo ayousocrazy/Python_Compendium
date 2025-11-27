5d344849c7cf07a690ce28842779381df1310bf4
ayousocrazy <ayousocrazy@gmail.com>
2025-11-27 18:02:24 +0545

    Add conditional.py

---
+++ Concepts/conditional.py
@@ -0,0 +1,11 @@
+"""
+We use if elif else for conditional statements
+"""
+
+num = 100231014
+if num % 7:
+    print(f"{num} is divisible by 7")
+elif num % 5:
+    print(f"{num} is divisible by 5")
+else:
+    print(f"{num} is not divisible by 7 and 5")
---
+++ Concepts/input.py
@@ -0,0 +1,9 @@
+"""
+Instead of giving a fixed value to a variable, input helps in getting dynamic values from user
+Note: the input is always in string
+"""
+
+example = input("Enter example input: ")
+num = int(input("Enter the integer input: "))
+decimal = float(input("Enter decimal input: "))
+expression = eval(input("Enter arithemetic expression: "))
