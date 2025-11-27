5c0095f05ce8018a244de50f8f23f9e4858896ca
ayousocrazy <ayousocrazy@gmail.com>
2025-11-27 18:39:19 +0545

    Add loop.py

--- Concepts/conditional.py
+++ Concepts/conditional.py
@@ -1,5 +1,7 @@
 """
 We use if elif else for conditional statements
+Or 
+Match case
 """
 
 num = 100231014
@@ -9,3 +11,17 @@ elif num % 5:
     print(f"{num} is divisible by 5")
 else:
     print(f"{num} is not divisible by 7 and 5")
+
+# -------------------------------------------------------------------------------------------------------------------------
+
+day = input("Enter day of week: ").lower()
+
+match day:
+    case 'sunday' | 'monday' | 'tuesday':
+        print(":(")
+    case 'wednesday' | 'thrusday':
+        print(":|")
+    case 'friday':
+        print(':)')
+    case _:
+        print("*3*")
---
+++ Concepts/loop.py
@@ -0,0 +1,31 @@
+"""
+There are two ways to loop
+while
+for
+"""
+
+# While loop needs a variable declaration before while, condition check on while and increment on loop 
+i = 0
+while i<5:
+    print("Ayousocrazy")
+    i +=1
+
+# For loop handles everything in the same line 
+for x in range(1, 10, 2):
+    print(x)
+
+fruits = ["Apple", "Banana", "Mango", "Orange", "Kiwi", "Tomato", "Papaya", "Cherry", "Fig", "Melon"] 
+
+for x in fruits:
+    if x.lower() == 'tomato':
+        break
+    print(x)
+else:
+    print("Loop Ended")
+# You can add a break to break out of the loop at any time 
+
+for x in range(1, 5):
+    print(x)
+else:
+    print("Loop Ended")
+# the else only execute when loop ends without break 
