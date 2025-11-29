c9a7db7e26ea35c5cf73bc2bef50341b07164987
ayousocrazy <ayousocrazy@gmail.com>
2025-11-29 20:49:48 +0545

    Add function.py

---
+++ Concepts/function.py
@@ -0,0 +1,42 @@
+"""
+You can call a function from anywhere in your program, and you can call it as many times as you want.
+"""
+
+# Function definition 
+def add(a, b): # these varaiables are formal arguments
+    result = a + b
+    return(result)
+
+# function call
+print(add(6, 5))  # these varaiables are actual arguments
+
+# -------------------------------------------------------------------------------------------------------------------------
+
+def product(num1, num2, num3):
+    print(num1 * num2 * num3)
+product(22, 33, 44) # this is called position argument method
+product(num2 = 22, num1 = 55, num3 = 66) # this is keyword argumet method
+
+def division(num1, num2 = 4):
+    print(num1 / num2)
+division(24) # this is called default argument method
+division(24, 3) # it overwrites the default value 
+
+def adda(*b):
+    s = 0
+    print(b)
+    for i in b:
+        s += i
+    print(s)
+adda(123, 456, 789) # this is called variable length arguments
+# This is used when we don't know the total number of value
+
+def info(name, **kwargs):
+    print(f"name: {name}")
+    for key, val in kwargs.items():
+        print(f"{key}: {val}")
+info("Ram Chand", age = 44, profession = "trader", DOB = "12 Jan 1999")
+# **  sign converts into dictionary and is used when we need the data key with values
+
+# -------------------------------------------------------------------------------------------------------------------------
+
