39c147601c06089727b67bfb0413e173810b6282
ayousocrazy <ayousocrazy@gmail.com>
2025-12-06 14:27:15 +0545

    Solve problem4

--- Problems/Intermediate/problem4.py
+++ Problems/Intermediate/problem4.py
@@ -11,3 +11,23 @@ Requirements:
 -You must use a list as a stack.
 -No using any library.
 """
+
+def is_balanced(s):
+    stack = []
+    pairs = {')': '(', '}': '{', ']': '['}
+
+    for ch in s:
+        if ch in "({[":
+            stack.append(ch)
+        else:
+            if not stack:
+                return False
+            if stack.pop() != pairs[ch]: 
+                return False
+
+    return len(stack) == 0
+
+
+# TESTS
+print("Test 1:")
+print(is_balanced("[{{{()}}}]"))
