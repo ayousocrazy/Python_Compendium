b2e597ae16de298711686d2d779e3bc3b4ba8e12
ayousocrazy <ayousocrazy@gmail.com>
2025-12-06 15:45:16 +0545

    Solve problem 6

--- Problems/Intermediate/problem6.py
+++ Problems/Intermediate/problem6.py
@@ -11,3 +11,15 @@ Requirement:
 -Must yield numbers infinitely.
 -No lists allowed.
 """
+
+def fibonacci():
+    num1 = 0
+    num2 = 1
+    while True:
+        yield num1
+        num1, num2 = num2, num1 + num2
+
+fib = fibonacci()
+
+for _ in range(10):
+    print(next(fib))
