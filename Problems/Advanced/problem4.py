9d83d1dc4d7f066dd42c0af8bee7344eb620b347
ayousocrazy <ayousocrazy@gmail.com>
2025-12-07 22:11:41 +0545

    Solve advance Problem4

--- Problems/Advanced/problem4.py
+++ Problems/Advanced/problem4.py
@@ -6,3 +6,18 @@ Usage:
     for index, value in my_enumerate(['a','b','c'], 5):
         print(index, value)
 """
+
+def my_enumerate(iterable, start=0):
+    current = start
+    it = iter(iterable)
+    
+    while True:
+        try:
+            value = next(it)
+        except StopIteration:
+            return
+        yield current, value
+        current += 1
+
+for index, value in my_enumerate(['a','b','c'], 5):
+    print(index, value)
