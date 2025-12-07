fc578e40d571f815146c4c225b940e211a673061
ayousocrazy <ayousocrazy@gmail.com>
2025-12-07 16:18:09 +0545

    Solve advance problem2

--- Problems/Advanced/problem2.py
+++ Problems/Advanced/problem2.py
@@ -9,3 +9,32 @@ Usage:
     for token in TokenStream("hello   world this is python"):
         print(token)
 """
+
+class TokenStream:
+    def __init__(self, txt):
+        self.text = txt
+        self.i = 0
+        self.n = len(txt)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.i < self.n and self.text[self.i] == " ":
+            self.i += 1
+
+        if self.i >= self.n:
+            raise StopIteration
+
+        word = ""
+        while self.i < self.n and self.text[self.i] != " ":
+            word += self.text[self.i]
+            self.i += 1
+
+        return word
+
+# TESTS
+print("Test:")
+txt = "   hello   world this            is python     "
+for token in TokenStream(txt):
+    print(token)
